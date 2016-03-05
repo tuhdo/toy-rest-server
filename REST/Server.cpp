8ecf8de9153ad1727caa782d6e6a51eaf501efd9
Tu Do <tuhdo@MacBook-Pro.local>
2016-03-06 05:07:49 +0700

    Now the question can appear on browser

--- REST/Server.cpp
+++ REST/Server.cpp
@@ -11,14 +11,23 @@ void Server::run() {
 
     for (;; ) {
         if (!server_conn.accept())
-            server_conn.respond(get_response);
-        std::cout << "question: " << compose_response() << std::endl;
+            server_conn.respond(compose_response());
     }
 }
 
 std::string Server::compose_response() {
     lisp_conn.connect();
     lisp_conn.send(server_conn.get_recv_msg().get_method() + "\n");
+    std::string response =
+        "HTTP/1.0 200 OK\r\n"
+        "Content-Length: 5\r\n"
+        "Content-Type: text/html\r\n\r\n"
+        "<html><body>Question: "
+        +  lisp_conn.receive() +
+        "<form method=\"PUT\" action=\"/answer\">"
+        "Input 2: <input type=\"text\" name=\"input_2\" /> <br/>"
+        "<input type=\"submit\" />"
+        "</form></body></html>";
 
-    return lisp_conn.receive();
+    return response;
 }
--- REST/Server.h
+++ REST/Server.h
@@ -3,19 +3,11 @@
 static const char *html_form =
     "<html><body>POST example."
     "<form method=\"PUT\" action=\"/answer\">"
-    "Input 2: <input type=\"text\" name=\"input_2\" /> <br/>"
+    "Answer: <input type=\"text\" name=\"answer\" /> <br/>"
     "<input type=\"submit\" />"
     "</form></body></html>";
 
-static const std::string get_response =
-    "HTTP/1.0 200 OK\r\n"
-"Content-Length: 5\r\n"
-"Content-Type: text/html\r\n\r\n"
-"<html><body>Question: "
-    "<form method=\"PUT\" action=\"/answer\">"
-"Input 2: <input type=\"text\" name=\"input_2\" /> <br/>"
-"<input type=\"submit\" />"
-"</form></body></html>";
+static const 
 
 class Server
 {
